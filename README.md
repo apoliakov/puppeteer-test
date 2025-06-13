492f39fc478d196fe7d91c8091f112b3e65f8f9d
apoliakov <alex.poliakov@dbos.dev>
2025-06-13 17:09:08 -0400

    Example

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # puppeteer-test
-Temp example using puppeteer with dbos!
+Temp example using puppeteer with dbos.
+
+Make sure to increase VM memory:
+```
+dbos-cloud app register -d <your_database>
+dbos-cloud app update --executors-memory-mib=2500
+dbos-cloud app deploy
+```
+
+Then visit `/screenshot` to see a snap of wikipedia.org
---
+++ dbos-node-starter/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+apt install -y \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libcups2 \
+  libdrm2 \
+  libgbm1 \
+  libgtk-3-0 \
+  libnspr4 \
+  libnss3 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  xdg-utils \
+  libasound2 \
+  libxss1 \
+  libx11-xcb1 \
+  libxcb-dri3-0 \
+  libxshmfence1 \
+  libx11-6 \
+  libxext6 \
+  libxfixes3 \
+  ca-certificates \
+  fonts-liberation \
+  libappindicator3-1 \
+  libnss3 \
+  lsb-release
